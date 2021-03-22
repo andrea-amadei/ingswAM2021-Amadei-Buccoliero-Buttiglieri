69b9f1d920af2fe89d43705290fa81dd04e4d0a9
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-03-22 23:32:36 +0100

    Added equal and hash methods

--- src/main/java/it/polimi/ingsw/gamematerials/BaseFlag.java
+++ src/main/java/it/polimi/ingsw/gamematerials/BaseFlag.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.gamematerials;
 
+import java.util.Objects;
+
 /**
  * The BaseFlag class represents a generic flag, only defined by its color
  */
@@ -19,5 +21,18 @@ public class BaseFlag {
      */
     public FlagColor getColor() {
         return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseFlag baseFlag = (BaseFlag) o;
+        return color == baseFlag.color;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color);
     }
 }
