0b1ed58e484dd3ef4627cf3cb981bc5721b06549
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-03-23 11:54:42 +0100

    Added DiscountHolder and related tests

---
+++ src/test/java/it/polimi/ingsw/DiscountHolderTest.java
@@ -0,0 +1,40 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.gamematerials.ResourceTypeSingleton;
+import it.polimi.ingsw.model.holder.DiscountHolder;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiscountHolderTest {
+
+    @Test
+    public void testMethods() {
+        DiscountHolder dh = new DiscountHolder();
+
+        assertEquals(dh.getDiscounts().size(), 0);
+
+        dh.addDiscount(ResourceTypeSingleton.getInstance().getGoldResource(), 1);
+        dh.addDiscount(ResourceTypeSingleton.getInstance().getShieldResource(), 1);
+        dh.addDiscount(ResourceTypeSingleton.getInstance().getGoldResource(), 2);
+
+        assertEquals(dh.getDiscounts().size(), 2);
+        assertEquals(dh.totalDiscountByResource(ResourceTypeSingleton.getInstance().getGoldResource()), 3);
+        assertEquals(dh.totalDiscountByResource(ResourceTypeSingleton.getInstance().getShieldResource()), 1);
+        assertEquals(dh.totalDiscountByResource(ResourceTypeSingleton.getInstance().getStoneResource()), 0);
+
+        dh.reset();
+
+        assertEquals(dh.getDiscounts().size(), 0);
+    }
+
+    @Test
+    public void testExceptions() {
+        DiscountHolder dh = new DiscountHolder();
+
+        assertThrows(NullPointerException.class, () -> dh.addDiscount(null, 1));
+        assertThrows(IllegalArgumentException.class,
+                () -> dh.addDiscount(ResourceTypeSingleton.getInstance().getGoldResource(), 0));
+        assertThrows(NullPointerException.class, () -> dh.totalDiscountByResource(null));
+    }
+}
