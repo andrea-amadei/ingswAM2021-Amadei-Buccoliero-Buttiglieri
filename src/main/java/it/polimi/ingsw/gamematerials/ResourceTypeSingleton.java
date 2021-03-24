00ed79e539342c7947d3e6188a6bed871cd9a3fc
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-03-24 20:16:52 +0100

    Added JavaDoc to ResourceTypeSingleton

--- src/main/java/it/polimi/ingsw/gamematerials/ResourceTypeSingleton.java
+++ src/main/java/it/polimi/ingsw/gamematerials/ResourceTypeSingleton.java
@@ -3,6 +3,10 @@ package it.polimi.ingsw.gamematerials;
 import java.util.Arrays;
 import java.util.HashSet;
 
+/**
+ * The ResourceTypeSingleton class implements a singleton design pattern.
+ * It can be used to get predefined instances of resources.
+ */
 public class ResourceTypeSingleton {
     private static final ResourceTypeSingleton instance = new ResourceTypeSingleton();
 
@@ -12,18 +16,38 @@ public class ResourceTypeSingleton {
     final ResourceSingle STONE = new ResourceSingle("Stone");
     final ResourceGroup ANY = new ResourceGroup("Any", new HashSet<>(Arrays.asList(GOLD, SERVANT, SHIELD, STONE)));
 
+    private ResourceTypeSingleton() {}
 
-    private ResourceTypeSingleton() {
-    }
-
+    /**
+     * Can be sued to obtain access to resources
+     * @return a initiated version of himself
+     */
     public static ResourceTypeSingleton getInstance(){
         return instance;
     }
 
+    /**
+     * @return gold resource
+     */
     public ResourceSingle getGoldResource() {return GOLD;}
+
+    /**
+     * @return servant resource
+     */
     public ResourceSingle getServantResource() {return SERVANT;}
+
+    /**
+     * @return shield resource
+     */
     public ResourceSingle getShieldResource() {return SHIELD;}
+
+    /**
+     * @return stone resource
+     */
     public ResourceSingle getStoneResource() {return STONE;}
 
+    /**
+     * @return the generic resource any, composed by gold, servant, shield and stone
+     */
     public ResourceGroup getAnyResource() {return ANY;}
 }
