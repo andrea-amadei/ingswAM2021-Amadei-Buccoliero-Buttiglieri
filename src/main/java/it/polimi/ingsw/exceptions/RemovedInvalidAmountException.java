b5ee2133b3bcb4372dcead10efa9480eb24ae4b5
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-03-25 18:06:52 +0100

    Removed RemovedInvalidAmountException and RemovedInvalidResourceException

--- src/main/java/it/polimi/ingsw/exceptions/RemovedInvalidAmountException.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.exceptions;
-
-public class RemovedInvalidAmountException extends RuntimeException{
-    public RemovedInvalidAmountException(String message) {
-        super(message);
-    }
-}
--- src/main/java/it/polimi/ingsw/exceptions/RemovedInvalidResourceException.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.exceptions;
-
-public class RemovedInvalidResourceException extends RuntimeException{
-    public RemovedInvalidResourceException(String message) {
-        super(message);
-    }
-}
