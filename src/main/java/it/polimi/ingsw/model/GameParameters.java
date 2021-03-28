a3cd1e0c8f9ff78b3e72a454e795875e076ffae8
Giorgio Natale Buttiglieri <10623930@polimi.it>
2021-03-28 17:34:44 +0200

    Added default values to GameParameters for the base cupboard

--- src/main/java/it/polimi/ingsw/model/GameParameters.java
+++ src/main/java/it/polimi/ingsw/model/GameParameters.java
@@ -1,5 +1,12 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.gamematerials.ResourceType;
+import it.polimi.ingsw.gamematerials.ResourceTypeSingleton;
+import it.polimi.ingsw.model.storage.Shelf;
+
+import java.util.Arrays;
+import java.util.List;
+
 public final class GameParameters {
     public static final int MAX_USERNAME_LENGTH = 30;
     public static final int MIN_USERNAME_LENGTH = 2;
@@ -7,7 +14,12 @@ public final class GameParameters {
     public static final int MAX_CARD_LEVEL = 3;
     public static final int MIN_CARD_LEVEL = 1;
 
-    public static final int BASE_CUPBOARDS_SHELVES = 3;
+    public static final List<String> BASE_CUPBOARD_SHELF_NAMES = Arrays.asList("BottomShelf", "MiddleShelf", "TopShelf");
+    public static final List<ResourceType> BASE_CUPBOARD_SHELF_TYPES = Arrays.asList(
+            ResourceTypeSingleton.getInstance().getAnyResource(),
+            ResourceTypeSingleton.getInstance().getAnyResource(),
+            ResourceTypeSingleton.getInstance().getAnyResource());
+    public static final List<Integer> BASE_CUPBOARD_SHELF_SIZES = Arrays.asList(3, 2, 1);
 
     public static final int UPGRADABLE_CRAFTING_NUMBER = 3;
 
