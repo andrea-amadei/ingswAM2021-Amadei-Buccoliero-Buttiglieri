97b0047e2bfd91526dd1560c537cd5f2106b6839
Giorgio Natale Buttiglieri <10623930@polimi.it>
2021-03-30 10:40:36 +0200

    Added javadoc to Storage

--- src/main/java/it/polimi/ingsw/model/storage/Storage.java
+++ src/main/java/it/polimi/ingsw/model/storage/Storage.java
@@ -5,12 +5,22 @@ import it.polimi.ingsw.model.GameParameters;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The comprehensive storage of a player. It contains a chest, the hand, a market basket and a cupboard.
+ * The chest stores all resources crafted in the production.
+ * The hand is a temporary container used by the player to tidy his/her cupboard.
+ * The market basket is the container in which resources are collected from the market
+ * The cupboard contains the base shelves and possibly the leader shelves
+ */
 public class Storage {
     private final BaseStorage chest;
     private final BaseStorage hand;
     private final BaseStorage marketBasket;
     private final Cupboard cupboard;
 
+    /**
+     * A new empty storage is created. The cupboard is initialized with the parameters stated in GameParameters class
+     */
     public Storage(){
         chest = new BaseStorage();
         hand = new BaseStorage();
@@ -25,18 +35,34 @@ public class Storage {
 
     }
 
+    /**
+     * Returns the chest of this storage
+     * @return the hand of this storage
+     */
     public BaseStorage getChest() {
         return chest;
     }
 
+    /**
+     * Returns the hand of this storage
+     * @return the hand of this storage
+     */
     public BaseStorage getHand() {
         return hand;
     }
 
+    /**
+     * Returns the market basket of this storage
+     * @return the market basket of this storage
+     */
     public BaseStorage getMarketBasket() {
         return marketBasket;
     }
 
+    /**
+     * Returns the cupboard of this storage
+     * @return the cupboard of this storage
+     */
     public Cupboard getCupboard() {
         return cupboard;
     }
