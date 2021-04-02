93bea4d2fde3bc9dc7452c8a9514eb7320b21f8a
Giorgio Natale Buttiglieri <10623930@polimi.it>
2021-04-02 23:24:45 +0200

    Added some parameters to GameParameters

--- src/main/java/it/polimi/ingsw/model/GameParameters.java
+++ src/main/java/it/polimi/ingsw/model/GameParameters.java
@@ -1,11 +1,13 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.gamematerials.MarbleColor;
 import it.polimi.ingsw.gamematerials.ResourceType;
 import it.polimi.ingsw.gamematerials.ResourceTypeSingleton;
-import it.polimi.ingsw.model.storage.Shelf;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public final class GameParameters {
     public static final int MAX_USERNAME_LENGTH = 30;
@@ -21,7 +23,19 @@ public final class GameParameters {
             ResourceTypeSingleton.getInstance().getAnyResource());
     public static final List<Integer> BASE_CUPBOARD_SHELF_SIZES = Arrays.asList(3, 2, 1);
 
-    public static int MAX_FLAG_LEVEL = 3;
+    public static final int MARKET_ROWS = 3;
+    public static final int MARKET_COLUMNS = 4;
+
+    public static final Map<MarbleColor, Integer> MARBLE_PER_COLOR =
+            new HashMap<MarbleColor, Integer>(){{
+                put(MarbleColor.BLUE, 2);
+                put(MarbleColor.WHITE, 4);
+                put(MarbleColor.GREY, 2);
+                put(MarbleColor.RED, 1);
+                put(MarbleColor.PURPLE, 2);
+                put(MarbleColor.YELLOW, 2);
+
+            }};
 
     public static final int UPGRADABLE_CRAFTING_NUMBER = 3;
 
