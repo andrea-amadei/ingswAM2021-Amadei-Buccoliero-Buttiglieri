f80de77dc26c4719e57ab58558921b47e9ddcb4e
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-04-06 21:37:45 +0200

    Added new IllegalRawConversionException

---
+++ src/main/java/it/polimi/ingsw/exceptions/IllegalRawConversionException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.exceptions;
+
+public class IllegalRawConversionException extends Exception {
+    public IllegalRawConversionException(String message) {
+        super(message);
+    }
+}
