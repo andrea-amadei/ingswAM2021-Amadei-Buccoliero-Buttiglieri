d2d2a291dd148df2f91150d21df8b68ca62a280d
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-04-06 17:17:22 +0200

    Added getResourceSingleByName and getResourceGroupByName methods

--- src/main/java/it/polimi/ingsw/gamematerials/ResourceTypeSingleton.java
+++ src/main/java/it/polimi/ingsw/gamematerials/ResourceTypeSingleton.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.gamematerials;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.NoSuchElementException;
 
 /**
  * The ResourceTypeSingleton class implements a singleton design pattern.
@@ -50,4 +51,47 @@ public class ResourceTypeSingleton {
      * @return the generic resource any, composed by gold, servant, shield and stone
      */
     public ResourceGroup getAnyResource() {return ANY;}
+
+    /**
+     * Returns the matching resource
+     * @param name the name of the single resource
+     * @return the resource single matching the given name
+     */
+    public ResourceSingle getResourceSingleByName(String name) {
+        if(name == null)
+            throw new NullPointerException();
+
+        name = name.toLowerCase();
+
+        switch (name) {
+            case "gold":
+                return GOLD;
+            case "servant":
+                return SERVANT;
+            case "shield":
+                return SHIELD;
+            case "stone":
+                return STONE;
+
+            default:
+                throw new NoSuchElementException("No such resource as \"" + name + "\"");
+        }
+    }
+
+    /**
+     * Returns the matching resource
+     * @param name the name of the group resource
+     * @return the resource group matching the given name
+     */
+    public ResourceGroup getResourceGroupByName(String name) {
+        if(name == null)
+            throw new NullPointerException();
+
+        name = name.toLowerCase();
+
+        if(name.equals("any"))
+            return ANY;
+
+        throw new NoSuchElementException("No such resource as \"" + name + "\"");
+    }
 }
