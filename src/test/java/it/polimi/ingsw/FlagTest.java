07d43a9b882fb2bfe33ed2aa9df3999c0116aff0
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-04-09 11:53:02 +0200

    Updated Flag tests

--- src/test/java/it/polimi/ingsw/FlagTest.java
+++ src/test/java/it/polimi/ingsw/FlagTest.java
@@ -3,11 +3,14 @@ package it.polimi.ingsw;
 import static org.junit.jupiter.api.Assertions.*;
 
 import it.polimi.ingsw.gamematerials.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Flag tests")
 public class FlagTest {
     @Test
-    public void test() {
+    @DisplayName("Construction")
+    public void constructionTest() {
         BaseFlag green = new BaseFlag(FlagColor.GREEN);
         assertSame(green.getColor(), FlagColor.GREEN);
 
@@ -17,5 +20,15 @@ public class FlagTest {
 
         assertThrows(IllegalArgumentException.class, () -> new LevelFlag(FlagColor.YELLOW, 4));
         assertThrows(NullPointerException.class, () -> new BaseFlag(null));
+    }
+
+    @Test
+    @DisplayName("To string")
+    public void toStringTest() {
+        BaseFlag green = new BaseFlag(FlagColor.GREEN);
+        LevelFlag blue2 = new LevelFlag(FlagColor.BLUE, 2);
+
+        assertEquals("BaseFlag{color=GREEN}", green.toString());
+        assertEquals("LevelFlag{color=BLUE, level=2}", blue2.toString());
     }
 }
