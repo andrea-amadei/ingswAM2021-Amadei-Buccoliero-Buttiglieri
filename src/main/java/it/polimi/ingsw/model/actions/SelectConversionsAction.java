910936b6e6b4ce7145f57c8d1e71a18e285c99b6
Giorgio Natale Buttiglieri <10623930@polimi.it>
2021-04-13 10:56:12 +0200

    Added SelectConversionsAction and related

---
+++ src/main/java/it/polimi/ingsw/model/actions/SelectConversionsAction.java
@@ -0,0 +1,139 @@
+package it.polimi.ingsw.model.actions;
+
+import it.polimi.ingsw.common.InfoPayload;
+import it.polimi.ingsw.common.Message;
+import it.polimi.ingsw.exceptions.IllegalActionException;
+import it.polimi.ingsw.exceptions.IllegalResourceTransferException;
+import it.polimi.ingsw.model.FaithPath;
+import it.polimi.ingsw.model.GameModel;
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.fsm.GameContext;
+import it.polimi.ingsw.model.holder.ConversionHolder;
+import it.polimi.ingsw.model.market.ConversionActuator;
+import it.polimi.ingsw.model.market.Marble;
+import it.polimi.ingsw.model.market.Market;
+import it.polimi.ingsw.server.Console;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+public class SelectConversionsAction implements Action{
+
+    private final String player;
+    private final List<Integer> actuatorsChoice;
+
+    /**
+     * Creates a new ConversionAction.
+     * @param player the player that wants to select the conversions
+     * @param actuatorsChoice the list of choices made by the player. In order to be executed, its length must
+     *                        equal the length of the picked marbles.
+     *                        Each element must be greater or equal to 0 and, in order to be executed, it must
+     *                        be smaller then the number of possible conversions of the corresponding marble
+     * @throws NullPointerException if either player or actuatorsChoice is null
+     * @throws IllegalArgumentException if some values of actuatorsChoice are negative
+     */
+    public SelectConversionsAction(String player, List<Integer> actuatorsChoice){
+        if(player == null || actuatorsChoice == null)
+            throw new NullPointerException();
+        if(actuatorsChoice.stream().anyMatch(x->x<0))
+            throw new IllegalArgumentException("Actuator choices can't be negative");
+        this.player = player;
+        this.actuatorsChoice = actuatorsChoice;
+    }
+
+    /**
+     * The player chooses the conversions to be used. For each marble, the chosen ConversionActuator
+     * adds the associated resources to the market basket and/or adds the associated faith points to the
+     * player.
+     *
+     * @param gameContext the current context of the game
+     * @return a list of messages that contains information about the change applied to the model
+     * @throws IllegalActionException if the action cannot be performed on the model
+     * @throws NullPointerException if gameContext is null
+     */
+    @Override
+    public List<Message> execute(GameContext gameContext) throws IllegalActionException {
+        if(gameContext == null)
+            throw new NullPointerException();
+
+        GameModel model = gameContext.getGameModel();
+        Market market = model.getMarket();
+        Player currentPlayer;
+
+        try{
+            currentPlayer = model.getPlayerById(player);
+        }catch(NoSuchElementException e){
+            throw new IllegalActionException(e.getMessage());
+        }
+
+        ConversionHolder conversionHolder = currentPlayer.getBoard().getConversionHolder();
+        FaithPath faithPath = model.getFaithPath();
+
+        //check if the actuatorsChoice is valid
+        List<Marble> selectedMarbles = market.getSelectedMarbles();
+        if(actuatorsChoice.size() != selectedMarbles.size())
+            throw new IllegalActionException("The sizes of actuatorsChoice and selectedMarble don't match");
+
+        for(int i = 0; i < actuatorsChoice.size(); i++){
+            List<ConversionActuator> playerConversionsPerMarble =
+                    conversionHolder.getActuatorsFromColor(selectedMarbles.get(i).getColor());
+            if(actuatorsChoice.get(i) < 0)
+                throw new IllegalActionException("The choice cannot be negative (should have been already checked)");
+            if(actuatorsChoice.get(i) > 0 && actuatorsChoice.get(i) >= playerConversionsPerMarble.size())
+                throw new IllegalActionException("The choice index is too high");
+        }
+
+        List<ConversionActuator> appliedConversions = new ArrayList<>();
+
+        //apply the selected conversions
+        for(int i = 0; i < selectedMarbles.size(); i++){
+
+            //apply the base conversion
+            if(conversionHolder.getActuatorsFromColor(selectedMarbles.get(i).getColor()).size() == 0) {
+                try {
+                    selectedMarbles.get(i).getBaseConversionActuator().actuateConversion(currentPlayer, faithPath);
+                    appliedConversions.add(selectedMarbles.get(i).getBaseConversionActuator());
+                } catch (IllegalResourceTransferException e) {
+                    Console.log("Failed to choose the correct conversions, but others may have already been applied");
+                    throw new IllegalActionException(e.getMessage());
+                }
+            }
+
+            //apply the selected conversion
+            else{
+                try {
+                    conversionHolder.getActuatorsFromColor(selectedMarbles.get(i).getColor()).get(actuatorsChoice.get(i))
+                            .actuateConversion(currentPlayer, faithPath);
+                    appliedConversions.add(conversionHolder.getActuatorsFromColor(selectedMarbles.get(i).getColor())
+                            .get(actuatorsChoice.get(i)));
+                } catch (IllegalResourceTransferException e) {
+                    Console.log("Failed to choose the correct conversions, but others may have already been applied");
+                    throw new IllegalActionException(e.getMessage());
+                }
+            }
+        }
+
+
+        //TODO: this will be replaced with a change payload
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("BasketUpdate: ").append(currentPlayer.getBoard().getStorage().getMarketBasket().getAllResources()).append("\n");
+        int addedFaith = 0;
+        for(ConversionActuator actuator : appliedConversions){
+            addedFaith += actuator.getFaith();
+        }
+
+        sb.append("FaithUpdate: added ").append(addedFaith).append(" faith points");
+
+        InfoPayload payload = new InfoPayload(sb.toString());
+        Message message = new Message(model.getPlayers().stream().map(Player::getUsername).collect(Collectors.toList()),
+                Collections.singletonList(payload));
+
+        return Collections.singletonList(message);
+
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/SelectConversionsActionTest.java
@@ -0,0 +1,115 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.exceptions.IllegalActionException;
+import it.polimi.ingsw.gamematerials.MarbleColor;
+import it.polimi.ingsw.gamematerials.ResourceSingle;
+import it.polimi.ingsw.gamematerials.ResourceTypeSingleton;
+import it.polimi.ingsw.model.GameModel;
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.actions.BuyFromMarketAction;
+import it.polimi.ingsw.model.actions.SelectConversionsAction;
+import it.polimi.ingsw.model.fsm.GameContext;
+import it.polimi.ingsw.model.market.ConversionActuator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SelectConversionsActionTest {
+
+    private GameContext gameContext;
+
+    private final ResourceSingle gold = ResourceTypeSingleton.getInstance().getGoldResource();
+    private final ResourceSingle servant = ResourceTypeSingleton.getInstance().getServantResource();
+    private final ResourceSingle shield = ResourceTypeSingleton.getInstance().getShieldResource();
+    private final ResourceSingle stone = ResourceTypeSingleton.getInstance().getStoneResource();
+
+
+    @BeforeEach
+    public void init(){
+        Player player1 = new Player("Paolo", 0);
+        Player player2 = new Player("Genoveffa", 1);
+        Player player3 = new Player("Gertrude", 2);
+
+        GameModel model = new GameModel(Arrays.asList(player1, player2, player3), new Random(3));
+
+        player2.getBoard().getConversionHolder().addConversionActuator(MarbleColor.WHITE,
+                new ConversionActuator(Collections.singletonList(servant), 0));
+
+        player2.getBoard().getConversionHolder().addConversionActuator(MarbleColor.WHITE,
+                new ConversionActuator(Collections.singletonList(gold), 0));
+        gameContext = new GameContext(model);
+    }
+
+    @Test
+    public void pickWithMultipleConversions(){
+        assertDoesNotThrow(()->new BuyFromMarketAction("Genoveffa", false, 2).execute(gameContext));
+        assertDoesNotThrow(()->new SelectConversionsAction("Genoveffa", Arrays.asList(0,0,1)).execute(gameContext));
+        Map<ResourceSingle, Integer> expectedBasket = new HashMap<>(){{
+            put(servant, 1);
+            put(gold, 1);
+        }};
+        assertEquals(expectedBasket,
+                gameContext.getGameModel().getPlayerById("Genoveffa").getBoard().getStorage()
+                        .getMarketBasket().getAllResources());
+    }
+
+    @Test
+    public void pickBaseConversions(){
+        assertDoesNotThrow(()->new BuyFromMarketAction("Paolo", false, 3).execute(gameContext));
+        assertDoesNotThrow(()->new SelectConversionsAction("Paolo", Arrays.asList(0,0,0)).execute(gameContext));
+        Map<ResourceSingle, Integer> expectedBasket = new HashMap<>(){{
+            put(gold, 1);
+            put(stone, 1);
+        }};
+
+        assertEquals(expectedBasket,
+                gameContext.getGameModel().getPlayerById("Paolo").getBoard().getStorage()
+                        .getMarketBasket().getAllResources());
+    }
+
+    @Test
+    public void pickNonValidConversions(){
+        assertDoesNotThrow(()->new BuyFromMarketAction("Paolo", false, 3).execute(gameContext));
+        assertThrows(IllegalActionException.class, ()->new SelectConversionsAction("Paolo", Arrays.asList(0,1,0)).execute(gameContext));
+    }
+
+    @Test
+    public void noSuchPlayerPickConversion(){
+        assertThrows(IllegalActionException.class, ()->new SelectConversionsAction("Boh", Arrays.asList(0,1,0)).execute(gameContext));
+    }
+
+    @Test
+    public void negativePickConversion(){
+        assertDoesNotThrow(()->new BuyFromMarketAction("Paolo", false, 3).execute(gameContext));
+        assertThrows(IllegalArgumentException.class, ()->new SelectConversionsAction("Paolo", Arrays.asList(0,-1,0)).execute(gameContext));
+    }
+
+    @Test
+    public void wrongSizeOfConversionChoices(){
+        assertDoesNotThrow(()->new BuyFromMarketAction("Genoveffa", false, 3).execute(gameContext));
+        assertThrows(IllegalActionException.class, ()->new SelectConversionsAction("Genoveffa", Arrays.asList(0,0)).execute(gameContext));
+    }
+
+    @Test
+    public void tooHighConversionChoice(){
+        assertDoesNotThrow(()->new BuyFromMarketAction("Genoveffa", false, 3).execute(gameContext));
+        assertThrows(IllegalActionException.class, ()->new SelectConversionsAction("Genoveffa", Arrays.asList(0,2,0)).execute(gameContext));
+    }
+
+    @Test
+    public void nullPlayerOrChoicesCreation(){
+        assertThrows(NullPointerException.class, ()->new SelectConversionsAction(null, Arrays.asList(0,2,0)));
+        assertThrows(NullPointerException.class, ()->new SelectConversionsAction("Paolo", null));
+    }
+
+    @Test
+    public void nullGameContextCreation(){
+        assertThrows(NullPointerException.class, ()->new SelectConversionsAction("Genoveffa", Arrays.asList(0,2,0)).execute(null));
+    }
+
+}
