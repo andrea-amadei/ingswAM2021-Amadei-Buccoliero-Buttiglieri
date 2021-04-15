127258e0bc95360398186fe2a1e2262ac52f6980
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-04-15 13:16:54 +0200

    Updated app to test parsing

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -13,6 +13,7 @@ public class App {
             JSONParser.parseLeaders(Paths.get("src/main/leaders.json"));
             JSONParser.parseCraftingCards(Paths.get("src/main/crafting.json"));
             JSONParser.parseBaseCrafting(Paths.get("src/main/crafting.json"));
+            JSONParser.parseFaithPath(Paths.get("src/main/faith.json"));
         } catch (Exception e) {
             Console.log(e.getClass().getSimpleName() + " - " + e.getMessage(),
                     Console.Severity.ERROR, Console.Format.RED);
