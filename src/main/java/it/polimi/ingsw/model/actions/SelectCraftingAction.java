5e2c2d1b0be13cf71ee332be2641ad32817e9e18
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-04-16 10:29:50 +0200

    Removed unused variable

--- src/main/java/it/polimi/ingsw/model/actions/SelectCraftingAction.java
+++ src/main/java/it/polimi/ingsw/model/actions/SelectCraftingAction.java
@@ -46,7 +46,6 @@ public class SelectCraftingAction implements Action {
             throw new NullPointerException();
 
         GameModel model = gameContext.getGameModel();
-        Production production;
         Player currentPlayer;
 
         try {
