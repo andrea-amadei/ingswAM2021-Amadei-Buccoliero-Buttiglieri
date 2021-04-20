42d72ed70829769f60666eb433f855e7292c3734
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-04-20 10:12:08 +0200

    Added BackAction

---
+++ src/main/java/it/polimi/ingsw/model/actions/BackAction.java
@@ -0,0 +1,64 @@
+package it.polimi.ingsw.model.actions;
+
+import it.polimi.ingsw.common.InfoPayload;
+import it.polimi.ingsw.common.Message;
+import it.polimi.ingsw.common.PayloadComponent;
+import it.polimi.ingsw.exceptions.IllegalActionException;
+import it.polimi.ingsw.model.GameModel;
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.fsm.GameContext;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+public class BackAction implements Action {
+
+    private final String player;
+
+    /**
+     * Creates a new BackAction. It is used to return to the previous state (if possible)
+     * @param player the username of the player that wants to go back
+     * @throws NullPointerException if player is null
+     */
+    public BackAction(String player){
+        if(player == null)
+            throw new NullPointerException();
+
+        this.player = player;
+    }
+
+    /**
+     * The player requests to go to the previous state, canceling everything he was doing
+     * until that point. The back action cannot always be activated since some states or actions are
+     * irreversible.
+     *
+     * @param gameContext the current context of the game
+     * @return a list of messages that contains information about the change applied to the model
+     * @throws IllegalActionException if the action cannot be performed on the model
+     * @throws NullPointerException   if gameContext is null
+     */
+    @Override
+    public List<Message> execute(GameContext gameContext) throws IllegalActionException {
+        if(gameContext == null)
+            throw new NullPointerException();
+
+        Player currentPlayer;
+        GameModel model = gameContext.getGameModel();
+
+        try{
+            currentPlayer = model.getPlayerById(player);
+        }catch(NoSuchElementException e){
+            throw new IllegalActionException(e.getMessage());
+        }
+
+        if(!currentPlayer.equals(gameContext.getCurrentPlayer()))
+            throw new IllegalActionException("The current player doesn't match the executor player");
+
+        List<String> targets = model.getPlayers().stream().map(Player::getUsername).collect(Collectors.toList());
+        PayloadComponent payload = new InfoPayload("Player " + player + " went back");
+        Message message = new Message(targets, Collections.singletonList(payload));
+        return Collections.singletonList(message);
+    }
+}
