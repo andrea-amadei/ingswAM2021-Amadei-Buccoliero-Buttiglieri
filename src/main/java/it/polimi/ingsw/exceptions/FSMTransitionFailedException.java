1178919d25fa6adbbd06b6e0c58824ae838a870e
Giorgio Natale Buttiglieri <10623930@polimi.it>
2021-04-30 12:02:10 +0200

    Now action-related exceptions can keep track of the messages to send to the clients

--- src/main/java/it/polimi/ingsw/exceptions/FSMTransitionFailedException.java
+++ src/main/java/it/polimi/ingsw/exceptions/FSMTransitionFailedException.java
@@ -1,12 +1,20 @@
 package it.polimi.ingsw.exceptions;
 
+import it.polimi.ingsw.common.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FSMTransitionFailedException extends Exception{
+
+    private final List<Message> gameMessages;
     /**
      * Constructs a new exception with {@code null} as its detail message.
      * The cause is not initialized, and may subsequently be initialized by a
      * call to {@link #initCause}.
      */
     public FSMTransitionFailedException() {
+        gameMessages = new ArrayList<>();
     }
 
     /**
@@ -19,5 +27,17 @@ public class FSMTransitionFailedException extends Exception{
      */
     public FSMTransitionFailedException(String message) {
         super(message);
+        gameMessages = new ArrayList<>();
     }
+
+    public FSMTransitionFailedException(List<Message> gameMessages){
+        this.gameMessages = gameMessages;
+    }
+
+    public FSMTransitionFailedException(List<Message> gameMessages, String message){
+        super(message);
+        this.gameMessages = gameMessages;
+    }
+
+    public List<Message> getGameMessages() {return gameMessages;}
 }
--- src/main/java/it/polimi/ingsw/exceptions/IllegalActionException.java
+++ src/main/java/it/polimi/ingsw/exceptions/IllegalActionException.java
@@ -1,12 +1,20 @@
 package it.polimi.ingsw.exceptions;
 
+import it.polimi.ingsw.common.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class IllegalActionException extends Exception{
+
+    private final List<Message> gameMessages;
     /**
      * Constructs a new exception with {@code null} as its detail message.
      * The cause is not initialized, and may subsequently be initialized by a
      * call to {@link #initCause}.
      */
     public IllegalActionException() {
+        gameMessages = new ArrayList<>();
     }
 
     /**
@@ -19,5 +27,21 @@ public class IllegalActionException extends Exception{
      */
     public IllegalActionException(String message) {
         super(message);
+        gameMessages = new ArrayList<>();
     }
+
+    public IllegalActionException(List<Message> gameMessages){
+        this.gameMessages = gameMessages;
+    }
+
+    public IllegalActionException(List<Message> gameMessages, String message){
+        super(message);
+        this.gameMessages = gameMessages;
+    }
+
+    public List<Message> getGameMessages(){
+        return gameMessages;
+    }
+
+
 }
