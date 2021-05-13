8264798e6938f1c53842ac07611c980a457f4755
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-05-13 16:00:45 +0200

    Updated console colors. Now they work as intended

--- src/main/java/it/polimi/ingsw/utils/BackgroundColor.java
+++ src/main/java/it/polimi/ingsw/utils/BackgroundColor.java
@@ -1,25 +1,25 @@
 package it.polimi.ingsw.utils;
 
 public enum BackgroundColor {
-    RESET("\033[0m"),
+    RESET("\u001B[0m"),
 
-    BLACK("\033[40m"),
-    RED("\033[41m"),
-    GREEN("\033[42m"),
-    YELLOW("\033[43m"),
-    BLUE("\033[44m"),
-    PURPLE("\033[45m"),
-    CYAN("\033[46m"),
-    WHITE("\033[47m"),
+    BLACK("\u001B[40m"),
+    RED("\u001B[41m"),
+    GREEN("\u001B[42m"),
+    YELLOW("\u001B[43m"),
+    BLUE("\u001B[44m"),
+    PURPLE("\u001B[45m"),
+    CYAN("\u001B[46m"),
+    WHITE("\u001B[47m"),
 
-    BLACK_BRIGHT("\033[0;100m"),
-    RED_BRIGHT("\033[0;101m"),
-    GREEN_BRIGHT("\033[0;102m"),
-    YELLOW_BRIGHT("\033[0;103m"),
-    BLUE_BRIGHT("\033[0;104m"),
-    PURPLE_BRIGHT("\033[0;105m"),
-    CYAN_BRIGHT("\033[0;106m"),
-    WHITE_BRIGHT("\033[0;107m");
+    BLACK_BRIGHT("\033[100m"),
+    RED_BRIGHT("\033[101m"),
+    GREEN_BRIGHT("\033[102m"),
+    YELLOW_BRIGHT("\033[103m"),
+    BLUE_BRIGHT("\033[104m"),
+    PURPLE_BRIGHT("\033[105m"),
+    CYAN_BRIGHT("\033[106m"),
+    WHITE_BRIGHT("\033[107m");
 
     private final String value;
 
