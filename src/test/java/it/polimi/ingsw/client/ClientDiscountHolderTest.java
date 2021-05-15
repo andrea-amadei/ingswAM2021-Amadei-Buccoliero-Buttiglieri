01b6b7d87d4f784e3772fad5a46b3f5e4c90cc95
Giorgio Natale Buttiglieri <10623930@polimi.it>
2021-05-15 17:03:22 +0200

    Implemented ClientDiscountHolder

---
+++ src/test/java/it/polimi/ingsw/client/ClientDiscountHolderTest.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.client;
+
+import it.polimi.ingsw.clientproto.model.ClientDiscountHolder;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ClientDiscountHolderTest {
+
+    @Test
+    public void creation(){
+        ClientDiscountHolder clientDiscountHolder = new ClientDiscountHolder();
+        assertEquals(0, clientDiscountHolder.getDiscounts().size());
+    }
+
+    @Test
+    public void addDiscount(){
+        ClientDiscountHolder clientDiscountHolder = new ClientDiscountHolder();
+        clientDiscountHolder.addDiscount("gold", 2);
+        assertEquals(new HashMap<String, Integer>(){{put("gold", 2);}}, clientDiscountHolder.getDiscounts());
+    }
+}
