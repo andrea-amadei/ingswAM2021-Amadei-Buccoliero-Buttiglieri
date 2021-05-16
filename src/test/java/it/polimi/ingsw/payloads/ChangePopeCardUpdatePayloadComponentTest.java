6daef5d00a7cbada1244a4b19b281f5aef606026
CaterinaBuccoliero <caterina.buccoliero@mail.polimi.it>
2021-05-16 10:37:07 +0200

    Tested serialization on ChangePopeCardUpdatePayloadComponent.

---
+++ src/test/java/it/polimi/ingsw/payloads/ChangePopeCardUpdatePayloadComponentTest.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.payloads;
+import it.polimi.ingsw.common.payload_components.PayloadComponent;
+import it.polimi.ingsw.model.holder.FaithHolder;
+import it.polimi.ingsw.parser.JSONSerializer;
+import it.polimi.ingsw.utils.PayloadFactory;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ChangePopeCardUpdatePayloadComponentTest {
+
+    @Test
+    public void correctlySerialized(){
+        PayloadComponent payload = PayloadFactory.changePopeCard("Ernestino",
+                FaithHolder.CheckpointStatus.ACTIVE,1);
+        String serialized = JSONSerializer.toJson(payload);
+
+        assertEquals("{\"type\":\"change_pope_card\",\"group\":\"update\",\"status\":\"ACTIVE\"," +
+                "\"index\":1,\"player\":\"Ernestino\"}", serialized);
+    }
+
+}
