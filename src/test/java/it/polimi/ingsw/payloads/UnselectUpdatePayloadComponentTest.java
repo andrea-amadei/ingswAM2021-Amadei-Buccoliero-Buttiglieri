865fc607271fb0d07d266688d29159f92b766572
CaterinaBuccoliero <caterina.buccoliero@mail.polimi.it>
2021-05-16 10:39:04 +0200

    Tested serialization on UnselectUpdatePayloadComponent.

---
+++ src/test/java/it/polimi/ingsw/payloads/UnselectUpdatePayloadComponentTest.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.payloads;
+import it.polimi.ingsw.common.payload_components.PayloadComponent;
+import it.polimi.ingsw.parser.JSONSerializer;
+import it.polimi.ingsw.utils.PayloadFactory;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UnselectUpdatePayloadComponentTest {
+
+    @Test
+    public void correctlySerialized(){
+        PayloadComponent payload = PayloadFactory.unselect("Ernestino", "market");
+        String serialized = JSONSerializer.toJson(payload);
+
+        assertEquals("{\"type\":\"unselect\",\"group\":\"update\",\"section\":\"market\",\"player\":" +
+                "\"Ernestino\"}", serialized);
+    }
+
+}
