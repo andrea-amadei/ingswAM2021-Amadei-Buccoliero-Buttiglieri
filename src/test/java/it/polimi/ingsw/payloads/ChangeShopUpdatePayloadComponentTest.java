8ad0e872ae2df5da48d2e8df03b302d9135ab7f4
CaterinaBuccoliero <caterina.buccoliero@mail.polimi.it>
2021-05-17 15:42:49 +0200

    Tested deserialization on ChangeShopUpdate.

--- src/test/java/it/polimi/ingsw/payloads/ChangeShopUpdatePayloadComponentTest.java
+++ src/test/java/it/polimi/ingsw/payloads/ChangeShopUpdatePayloadComponentTest.java
@@ -1,4 +1,7 @@
 package it.polimi.ingsw.payloads;
+import it.polimi.ingsw.clientproto.network.ServerNetworkObject;
+import it.polimi.ingsw.clientproto.parser.ClientDeserializer;
+import it.polimi.ingsw.clientproto.updates.ChangeShopUpdate;
 import it.polimi.ingsw.common.payload_components.PayloadComponent;
 import it.polimi.ingsw.parser.JSONSerializer;
 import it.polimi.ingsw.utils.PayloadFactory;
@@ -17,6 +20,21 @@ public class ChangeShopUpdatePayloadComponentTest {
 
         assertEquals("{\"type\":\"change_shop\",\"group\":\"update\",\"x\":1,\"y\":2,\"id\":4}",
                 serialized);
+    }
+
+    @Test
+    public void correctlyDeserialized(){
+        String serialized = "{\"type\":\"change_shop\",\"group\":\"update\",\"x\":1,\"y\":2,\"id\":4}";
+        ServerNetworkObject serverNetworkObject = ClientDeserializer.getServerNetworkObject(serialized);
+
+        assertTrue(serverNetworkObject instanceof ChangeShopUpdate);
+
+        ChangeShopUpdate update = ((ChangeShopUpdate)serverNetworkObject);
+        update.checkFormat();
+
+        assertEquals(1, update.getX());
+        assertEquals(2, update.getY());
+        assertEquals(4, update.getId());
     }
 
 }
