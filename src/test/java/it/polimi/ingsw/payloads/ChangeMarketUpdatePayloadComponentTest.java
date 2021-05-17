addda55d8af6e7ffababcee058c02b227e01a606
CaterinaBuccoliero <caterina.buccoliero@mail.polimi.it>
2021-05-17 15:41:46 +0200

    Tested deserialization on ChangeMarketUpdate.

--- src/test/java/it/polimi/ingsw/payloads/ChangeMarketUpdatePayloadComponentTest.java
+++ src/test/java/it/polimi/ingsw/payloads/ChangeMarketUpdatePayloadComponentTest.java
@@ -1,9 +1,13 @@
 package it.polimi.ingsw.payloads;
+import it.polimi.ingsw.clientproto.network.ServerNetworkObject;
+import it.polimi.ingsw.clientproto.parser.ClientDeserializer;
+import it.polimi.ingsw.clientproto.updates.ChangeMarketUpdate;
 import it.polimi.ingsw.common.payload_components.PayloadComponent;
 import it.polimi.ingsw.model.market.Market;
 import it.polimi.ingsw.parser.JSONSerializer;
 import it.polimi.ingsw.parser.raw.RawMarket;
 import it.polimi.ingsw.utils.PayloadFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
@@ -14,16 +18,38 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ChangeMarketUpdatePayloadComponentTest {
 
+    private RawMarket rawMarket;
+
+    @BeforeEach
+    public void init(){
+        Market market = new Market(new Random(17));
+        rawMarket = new RawMarket(market);
+    }
+
     @Test
     public void correctlySerialized(){
-        Market market = new Market(new Random(17));
-        RawMarket rawMarket = new RawMarket(market);
         PayloadComponent payload = PayloadFactory.changeMarket(rawMarket);
         String serialized = JSONSerializer.toJson(payload);
 
         assertEquals("{\"type\":\"change_market\",\"group\":\"update\",\"market\":" +
                 "{\"marbles\":[\"WHITE\",\"RED\",\"PURPLE\",\"WHITE\",\"GREY\",\"YELLOW\",\"WHITE\"," +
                 "\"BLUE\",\"GREY\",\"WHITE\",\"BLUE\",\"YELLOW\"],\"odd\":\"PURPLE\"}}", serialized);
+    }
+
+    @Test
+    public void correctlyDeserialized(){
+        String serialized = "{\"type\":\"change_market\",\"group\":\"update\",\"market\":" +
+                "{\"marbles\":[\"WHITE\",\"RED\",\"PURPLE\",\"WHITE\",\"GREY\",\"YELLOW\",\"WHITE\"," +
+                "\"BLUE\",\"GREY\",\"WHITE\",\"BLUE\",\"YELLOW\"],\"odd\":\"PURPLE\"}}";
+        ServerNetworkObject serverNetworkObject = ClientDeserializer.getServerNetworkObject(serialized);
+
+        assertTrue(serverNetworkObject instanceof ChangeMarketUpdate);
+
+        ChangeMarketUpdate update = ((ChangeMarketUpdate)serverNetworkObject);
+        update.checkFormat();
+
+        assertEquals(rawMarket.getOdd(), update.getMarket().getOdd());
+        assertEquals(rawMarket.getMarbles(), update.getMarket().getMarbles());
     }
 
 }
