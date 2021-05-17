1a1d918d5c44b1d3188ca0e899a06a7e394ab181
CaterinaBuccoliero <caterina.buccoliero@mail.polimi.it>
2021-05-17 15:48:15 +0200

    Tested deserialization on ChangeCurrentPlayerUpdatePayloadComponentTest.

---
+++ src/test/java/it/polimi/ingsw/payloads/ChangeCurrentPlayerUpdatePayloadComponentTest.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.payloads;
+import it.polimi.ingsw.clientproto.network.ServerNetworkObject;
+import it.polimi.ingsw.clientproto.parser.ClientDeserializer;
+import it.polimi.ingsw.clientproto.updates.ChangeCurrentPlayerUpdate;
+import it.polimi.ingsw.common.payload_components.PayloadComponent;
+import it.polimi.ingsw.parser.JSONSerializer;
+import it.polimi.ingsw.utils.PayloadFactory;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ChangeCurrentPlayerUpdatePayloadComponentTest {
+
+    @Test
+    public void correctlySerialized(){
+        PayloadComponent payload = PayloadFactory.changeCurrentPlayer("Genoveffa");
+        String serialized = JSONSerializer.toJson(payload);
+
+        assertEquals("{\"type\":\"change_current_player\",\"group\":\"update\",\"newPlayer\":\"Genoveffa\"}",
+                serialized);
+    }
+
+    @Test
+    public void correctlyDeserialized(){
+        String serialized = "{\"type\":\"change_current_player\",\"group\":\"update\",\"newPlayer\":\"Genoveffa\"}";
+        ServerNetworkObject serverNetworkObject = ClientDeserializer.getServerNetworkObject(serialized);
+
+        assertTrue(serverNetworkObject instanceof ChangeCurrentPlayerUpdate);
+
+        ChangeCurrentPlayerUpdate update = ((ChangeCurrentPlayerUpdate)serverNetworkObject);
+        update.checkFormat();
+
+        assertEquals("Genoveffa", update.getNewPlayer());
+    }
+
+}
