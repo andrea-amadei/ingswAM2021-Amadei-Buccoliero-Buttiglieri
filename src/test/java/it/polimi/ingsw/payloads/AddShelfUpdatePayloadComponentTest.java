9ba4e049d614e18ddb0fe0e39cd164de16bc6a0a
CaterinaBuccoliero <caterina.buccoliero@mail.polimi.it>
2021-05-17 15:39:24 +0200

    Tested deserialization on AddShelfUpdate.

--- src/test/java/it/polimi/ingsw/payloads/AddShelfUpdatePayloadComponentTest.java
+++ src/test/java/it/polimi/ingsw/payloads/AddShelfUpdatePayloadComponentTest.java
@@ -1,4 +1,7 @@
 package it.polimi.ingsw.payloads;
+import it.polimi.ingsw.clientproto.network.ServerNetworkObject;
+import it.polimi.ingsw.clientproto.parser.ClientDeserializer;
+import it.polimi.ingsw.clientproto.updates.AddShelfUpdate;
 import it.polimi.ingsw.common.payload_components.PayloadComponent;
 import it.polimi.ingsw.parser.JSONSerializer;
 import it.polimi.ingsw.utils.PayloadFactory;
@@ -16,8 +19,25 @@ public class AddShelfUpdatePayloadComponentTest {
                 "gold", 1);
         String serialized = JSONSerializer.toJson(payload);
 
-        assertEquals("{\"type\":\"add_shelf\",\"group\":\"update\",\"id\":\"LeaderShelfPaolo\",\"resource\":\"gold\",\"index\":1,\"player\":\"Ernestino\"}",
-                serialized);
+        assertEquals("{\"type\":\"add_shelf\",\"group\":\"update\",\"id\":\"LeaderShelfPaolo\",\"resource\":" +
+                        "\"gold\",\"index\":1,\"player\":\"Ernestino\"}", serialized);
+    }
+
+    @Test
+    public void correctlyDeserialized(){
+        String serialized = "{\"type\":\"add_shelf\",\"group\":\"update\",\"id\":\"LeaderShelfPaolo\",\"resource\":" +
+                "\"gold\",\"index\":1,\"player\":\"Ernestino\"}";
+        ServerNetworkObject serverNetworkObject = ClientDeserializer.getServerNetworkObject(serialized);
+
+        assertTrue(serverNetworkObject instanceof AddShelfUpdate);
+
+        AddShelfUpdate update = ((AddShelfUpdate)serverNetworkObject);
+        update.checkFormat();
+
+        assertEquals("Ernestino", update.getPlayer());
+        assertEquals("LeaderShelfPaolo", update.getId());
+        assertEquals("gold", update.getResource());
+        assertEquals(1, update.getIndex());
     }
 
 }
