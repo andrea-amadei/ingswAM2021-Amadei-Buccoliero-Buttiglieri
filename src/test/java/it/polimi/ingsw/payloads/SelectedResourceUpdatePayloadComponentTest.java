954f5fd61eddc97890022d0bbc999fdec7ea601e
CaterinaBuccoliero <caterina.buccoliero@mail.polimi.it>
2021-05-17 15:43:21 +0200

    Tested deserialization on SelectedResourceUpdate.

--- src/test/java/it/polimi/ingsw/payloads/SelectedResourceUpdatePayloadComponentTest.java
+++ src/test/java/it/polimi/ingsw/payloads/SelectedResourceUpdatePayloadComponentTest.java
@@ -1,4 +1,7 @@
 package it.polimi.ingsw.payloads;
+import it.polimi.ingsw.clientproto.network.ServerNetworkObject;
+import it.polimi.ingsw.clientproto.parser.ClientDeserializer;
+import it.polimi.ingsw.clientproto.updates.SelectedResourceUpdate;
 import it.polimi.ingsw.common.payload_components.PayloadComponent;
 import it.polimi.ingsw.parser.JSONSerializer;
 import it.polimi.ingsw.utils.PayloadFactory;
@@ -18,6 +21,23 @@ public class SelectedResourceUpdatePayloadComponentTest {
 
         assertEquals("{\"type\":\"selected_resource\",\"group\":\"update\",\"container_id\":\"Chest\",\"resource\"" +
                 ":\"gold\",\"amount\":2,\"player\":\"Ernestino\"}", serialized);
+    }
+
+    @Test
+    public void correctlyDeserialized(){
+        String serialized = "{\"type\":\"selected_resource\",\"group\":\"update\",\"container_id\":\"Chest\",\"resource\"" +
+                ":\"gold\",\"amount\":2,\"player\":\"Ernestino\"}";
+        ServerNetworkObject serverNetworkObject = ClientDeserializer.getServerNetworkObject(serialized);
+
+        assertTrue(serverNetworkObject instanceof SelectedResourceUpdate);
+
+        SelectedResourceUpdate update = ((SelectedResourceUpdate)serverNetworkObject);
+        update.checkFormat();
+
+        assertEquals("Ernestino", update.getPlayer());
+        assertEquals("Chest", update.getContainerId());
+        assertEquals("gold", update.getResource());
+        assertEquals(2, update.getAmount());
     }
 
 }
