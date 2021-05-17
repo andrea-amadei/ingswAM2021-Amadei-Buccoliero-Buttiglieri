5f632a51bb3fe809ead1f35876865e9f458408e9
CaterinaBuccoliero <caterina.buccoliero@mail.polimi.it>
2021-05-17 15:47:26 +0200

    Added SelectedShopCardUpdate.

---
+++ src/main/java/it/polimi/ingsw/clientproto/updates/SelectedShopCardUpdate.java
@@ -0,0 +1,44 @@
+package it.polimi.ingsw.clientproto.updates;
+
+import it.polimi.ingsw.clientproto.model.ClientModel;
+
+public class SelectedShopCardUpdate implements Update{
+
+    private final int x;
+    private final int y;
+    private final String player;
+
+    public SelectedShopCardUpdate(int x, int y, String player){
+        this.x = x;
+        this.y = y;
+        this.player = player;
+        checkFormat();
+    }
+
+    @Override
+    public void apply(ClientModel client) {
+
+    }
+
+    @Override
+    public void checkFormat() {
+        if(x < 0)
+            throw new IllegalArgumentException("x coordinate cannot be negative");
+        if(y < 0)
+            throw new IllegalArgumentException("y coordinate cannot be negative");
+        if(player == null)
+            throw new NullPointerException("Pointer to player is null");
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+}
