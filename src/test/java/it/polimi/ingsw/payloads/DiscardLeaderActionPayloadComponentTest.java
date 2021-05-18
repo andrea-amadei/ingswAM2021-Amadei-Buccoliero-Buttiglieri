eef6e1587b702121346df95e9a55e2021f59f843
CaterinaBuccoliero <caterina.buccoliero@mail.polimi.it>
2021-05-18 13:09:09 +0200

    Tested serialization and deserialization on DiscardLeaderActionPayloadComponent.

---
+++ src/test/java/it/polimi/ingsw/payloads/DiscardLeaderActionPayloadComponentTest.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.payloads;
+import it.polimi.ingsw.common.payload_components.PayloadComponent;
+import it.polimi.ingsw.common.payload_components.groups.actions.DiscardLeaderActionPayloadComponent;
+import it.polimi.ingsw.model.actions.DiscardLeaderAction;
+import it.polimi.ingsw.parser.JSONParser;
+import it.polimi.ingsw.parser.JSONSerializer;
+import it.polimi.ingsw.server.clienthandling.ClientNetworkObject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class DiscardLeaderActionPayloadComponentTest {
+
+    @Test
+    public void correctlySerialized(){
+        PayloadComponent payload = new DiscardLeaderActionPayloadComponent("Ernestino", 1);
+        String serialized = JSONSerializer.toJson(payload);
+
+        assertEquals("{\"type\":\"discard_leader\",\"group\":\"action\",\"leaderID\":1,\"player\":\"Ernestino\"}",
+                serialized);
+    }
+
+    @Test
+    public void correctlyDeserialized(){
+        String serialized = "{\"type\":\"discard_leader\",\"group\":\"action\",\"leaderID\":1,\"player\":\"Ernestino\"}";
+        ClientNetworkObject clientNetworkObject = JSONParser.getClientNetworkObject(serialized);
+
+        assertTrue(clientNetworkObject instanceof DiscardLeaderAction);
+
+        DiscardLeaderAction action = ((DiscardLeaderAction)clientNetworkObject);
+
+        assertEquals("Ernestino", action.getSender());
+        assertEquals(1, action.getLeaderID());
+    }
+
+}
