055e29145996b1c810d0d14fdccf22b4dba4b23c
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-05-21 12:22:32 +0200

    Added CliBuilder class

---
+++ src/main/java/it/polimi/ingsw/client/cli/CliBuilder.java
@@ -0,0 +1,44 @@
+package it.polimi.ingsw.client.cli;
+
+import it.polimi.ingsw.client.cli.framework.CliFramework;
+import it.polimi.ingsw.client.cli.framework.elements.Frame;
+import it.polimi.ingsw.client.cli.updaters.*;
+import it.polimi.ingsw.client.model.ClientModel;
+import it.polimi.ingsw.client.model.ClientPlayer;
+
+public final class CliBuilder {
+    public void createStartFrame(CliFramework framework) {
+        // TODO
+    }
+
+    public static void createGameFrames(CliFramework framework, ClientModel model) {
+        Frame frame;
+        ClientPlayer player;
+
+        for(int i = 0; i < model.getPlayers().size(); i++) {
+            frame = new Frame("player_" + (i + 1));
+            player = model.getPlayers().get(i);
+
+            new FaithPathCliUpdater(player.getFaithPath(), frame);
+
+            new DiscountHolderCliUpdater(player.getDiscountHolder(), frame);
+            new FlagHolderCliUpdater(player.getFlagHolder(), frame);
+
+            new ProductionCliUpdater(player.getProduction(), frame);
+
+            new BaseStorageCliUpdater(player.getHand(), frame, 19, 85, "Hand", false);
+            new BaseStorageCliUpdater(player.getChest(), frame, 19, 108, "Chest", false);
+            new BaseStorageCliUpdater(player.getMarketBasket(), frame, 19, 62, "Market Basket", true);
+
+            new LeaderCardsCliUpdater(player.getLeaderCards(), frame);
+
+            new PlayerCliUpdater(player, frame);
+
+            framework.addFrame(frame);
+        }
+
+        frame = new Frame("global");
+        new MarketCliUpdater(model.getMarket(), frame);
+        framework.addFrame(frame);
+    }
+}
