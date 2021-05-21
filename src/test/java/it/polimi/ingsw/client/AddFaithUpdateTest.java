736ca542b3cd744be978a4282809c1b798783930
CaterinaBuccoliero <caterina.buccoliero@mail.polimi.it>
2021-05-21 13:12:57 +0200

    Tested Updates.

---
+++ src/test/java/it/polimi/ingsw/client/AddFaithUpdateTest.java
@@ -0,0 +1,47 @@
+package it.polimi.ingsw.client;
+import it.polimi.ingsw.client.model.ClientModel;
+import it.polimi.ingsw.client.updates.AddFaithUpdate;
+import it.polimi.ingsw.exceptions.ParserException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AddFaithUpdateTest {
+
+    private ClientModel clientModel;
+
+    @BeforeEach
+    public void init() throws IOException, ParserException {
+        clientModel = new ClientModel();
+
+        List<String> usernames = new ArrayList<>();
+        usernames.add("Ernestino");
+        usernames.add("Giuseppa");
+        usernames.add("Domenica");
+
+        String configJSON = Files.readString(Path.of("src/main/config.json"));
+        String craftingJSON = Files.readString(Path.of("src/main/crafting.json"));
+        String faithJSON = Files.readString(Path.of("src/main/faith.json"));
+        String leadersJSON = Files.readString(Path.of("src/main/leaders.json"));
+
+        ClientGameBuilder.buildGame(clientModel, usernames, configJSON, craftingJSON, faithJSON, leadersJSON);
+    }
+
+    @Test
+    public void correctApply(){
+        AddFaithUpdate update = new AddFaithUpdate(3, "Ernestino");
+        update.apply(clientModel);
+
+        assertEquals(3, clientModel.getPlayerByName("Ernestino").getFaithPath().getFaithPoints());
+    }
+
+}
---
+++ src/test/java/it/polimi/ingsw/client/SelectedResourceUpdateTest.java
@@ -0,0 +1,54 @@
+package it.polimi.ingsw.client;
+
+import it.polimi.ingsw.client.model.ClientModel;
+import it.polimi.ingsw.client.updates.SelectedResourceUpdate;
+import it.polimi.ingsw.exceptions.ParserException;
+import it.polimi.ingsw.parser.raw.RawStorage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SelectedResourceUpdateTest {
+
+    private ClientModel clientModel;
+
+    @BeforeEach
+    public void init() throws IOException, ParserException {
+        clientModel = new ClientModel();
+
+        List<String> usernames = new ArrayList<>();
+        usernames.add("Ernestino");
+        usernames.add("Giuseppa");
+        usernames.add("Domenica");
+
+        String configJSON = Files.readString(Path.of("src/main/config.json"));
+        String craftingJSON = Files.readString(Path.of("src/main/crafting.json"));
+        String faithJSON = Files.readString(Path.of("src/main/faith.json"));
+        String leadersJSON = Files.readString(Path.of("src/main/leaders.json"));
+
+        ClientGameBuilder.buildGame(clientModel, usernames, configJSON, craftingJSON, faithJSON, leadersJSON);
+
+        RawStorage delta = new RawStorage("BottomShelf", new HashMap<>(){{put("servant", 3);}});
+        clientModel.getPlayerByName("Ernestino").getClientShelfById("BottomShelf").changeResources(delta);
+    }
+
+    @Test
+    public void correctApply(){
+        SelectedResourceUpdate update = new SelectedResourceUpdate("BottomShelf", "servant",
+                3, "Ernestino");
+        update.apply(clientModel);
+
+        assertEquals(3, clientModel.getPlayerByName("Ernestino").getClientShelfById("BottomShelf")
+                .getSelectedResources().get("servant"));
+    }
+
+}
