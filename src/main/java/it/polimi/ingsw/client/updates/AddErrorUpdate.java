59ad88b38c7f328487edaa48fa4c30bef2ac6922
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-05-26 11:50:47 +0200

    Fixed parsing bug that prevented deserialization

--- src/main/java/it/polimi/ingsw/client/updates/AddErrorUpdate.java
+++ src/main/java/it/polimi/ingsw/client/updates/AddErrorUpdate.java
@@ -1,8 +1,10 @@
 package it.polimi.ingsw.client.updates;
 
+import com.google.gson.annotations.SerializedName;
 import it.polimi.ingsw.client.model.ClientModel;
 
-public class AddErrorUpdate implements Update{
+public class AddErrorUpdate implements Update {
+    @SerializedName(value = "message", alternate = "error")
     private final String error;
 
     public AddErrorUpdate(String error) {
