5263e307b762f2005dd2bb58fec13c37987be07b
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-06-09 11:11:27 +0200

    Optimized imports

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,39 +1,10 @@
 package it.polimi.ingsw;
 
-import it.polimi.ingsw.client.ClientGameBuilder;
 import it.polimi.ingsw.client.ClientMain;
-import it.polimi.ingsw.client.cli.CliBuilder;
-import it.polimi.ingsw.client.cli.framework.CliFramework;
-import it.polimi.ingsw.client.cli.framework.OutputHandler;
-import it.polimi.ingsw.client.cli.framework.elements.*;
-import it.polimi.ingsw.client.cli.updaters.*;
-import it.polimi.ingsw.client.model.*;
-import it.polimi.ingsw.exceptions.ParserException;
 import it.polimi.ingsw.exceptions.UnableToDrawElementException;
-import it.polimi.ingsw.gamematerials.*;
-import it.polimi.ingsw.model.FaithPathGroup;
-import it.polimi.ingsw.model.FaithPathTile;
-import it.polimi.ingsw.model.holder.FaithHolder;
-import it.polimi.ingsw.model.leader.*;
-import it.polimi.ingsw.model.market.ConversionActuator;
-import it.polimi.ingsw.model.market.Market;
-import it.polimi.ingsw.model.production.Crafting;
-import it.polimi.ingsw.model.production.CraftingCard;
-import it.polimi.ingsw.model.production.UpgradableCrafting;
-import it.polimi.ingsw.model.storage.BaseStorage;
-import it.polimi.ingsw.model.storage.Shelf;
-import it.polimi.ingsw.parser.JSONParser;
-import it.polimi.ingsw.parser.raw.*;
-import it.polimi.ingsw.server.Logger;
 import it.polimi.ingsw.server.ServerMain;
-import it.polimi.ingsw.utils.BackgroundColor;
-import it.polimi.ingsw.utils.ForegroundColor;
 
-import java.io.IOException;
-import java.nio.file.Paths;
-
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 public class App {
 
