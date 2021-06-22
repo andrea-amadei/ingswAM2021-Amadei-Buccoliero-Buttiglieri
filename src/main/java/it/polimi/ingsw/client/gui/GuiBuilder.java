10bf98f4f791c37d24819703f0c93818fef648d7
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-06-22 23:23:45 +0200

    Updated new GuiBuilder

--- src/main/java/it/polimi/ingsw/client/gui/GuiBuilder.java
+++ src/main/java/it/polimi/ingsw/client/gui/GuiBuilder.java
@@ -1,8 +1,10 @@
 package it.polimi.ingsw.client.gui;
 
 import it.polimi.ingsw.client.ServerHandler;
+import it.polimi.ingsw.client.gui.controllers.BoardGuiController;
 import it.polimi.ingsw.client.gui.controllers.LobbyGuiController;
 import it.polimi.ingsw.client.gui.updaters.LobbyPersonalDataGuiUpdater;
+import it.polimi.ingsw.client.gui.updaters.ShopGuiUpdater;
 import it.polimi.ingsw.client.model.ClientModel;
 import it.polimi.ingsw.utils.Pair;
 import it.polimi.ingsw.utils.ResourceLoader;
@@ -11,17 +13,31 @@ import javafx.scene.Scene;
 
 public final class GuiBuilder {
 
-    public static void createStartScene(GuiSceneManager sceneManager, ClientModel model, ServerHandler serverHandler) {
+    public static void createStartScene(GuiSceneManager sceneManager, ClientModel clientModel, ServerHandler serverHandler) {
         Pair<Parent, ?> sceneAndController = ResourceLoader.loadFXML("jfx/start.fxml");
 
         Scene scene = new Scene(sceneAndController.getFirst());
         LobbyGuiController lobbyController = (LobbyGuiController) sceneAndController.getSecond();
 
-        lobbyController.init(model, serverHandler, sceneManager);
-
-        new LobbyPersonalDataGuiUpdater(model.getPersonalData(), scene);
+        lobbyController.init(clientModel, serverHandler, sceneManager);
 
         sceneManager.addScene("start", scene);
         sceneManager.setActiveScene("start");
+
+        new LobbyPersonalDataGuiUpdater(clientModel.getPersonalData(), scene);
+    }
+
+    public static void createGameScene(GuiSceneManager sceneManager, ClientModel clientModel, ServerHandler serverHandler) {
+        Pair<Parent, ?> sceneAndController = ResourceLoader.loadFXML("jfx/board.fxml");
+
+        Scene scene = new Scene(sceneAndController.getFirst());
+        BoardGuiController boardController = (BoardGuiController) sceneAndController.getSecond();
+
+        boardController.init(clientModel, serverHandler, sceneManager);
+
+        sceneManager.addScene("board", scene);
+        sceneManager.setActiveScene("board");
+
+        boardController.boardSetup();
     }
 }
