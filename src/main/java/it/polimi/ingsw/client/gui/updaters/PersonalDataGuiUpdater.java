80f7a287edb3413613f71efe3f36a0f24f2434a6
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-06-22 23:27:36 +0200

    Added PersonalDataGuiUpdater

---
+++ src/main/java/it/polimi/ingsw/client/gui/updaters/PersonalDataGuiUpdater.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.client.gui.updaters;
+
+import it.polimi.ingsw.client.gui.nodes.ScoreboardBox;
+import it.polimi.ingsw.client.model.PersonalData;
+import it.polimi.ingsw.client.observables.Listener;
+
+public class PersonalDataGuiUpdater implements Listener<PersonalData> {
+
+    // TODO: add messages and errors
+    public PersonalDataGuiUpdater(ScoreboardBox scoreboardBox, PersonalData personalData) {
+    }
+
+    @Override
+    public void update(PersonalData personalData) {
+
+    }
+}
