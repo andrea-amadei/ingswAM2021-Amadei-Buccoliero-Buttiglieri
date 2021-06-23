1e394775b875f07ddd46696c4856257e2d427c5d
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-06-24 01:14:10 +0200

    Added new StorageGuiUpdater

---
+++ src/main/java/it/polimi/ingsw/client/gui/updaters/StorageGuiUpdater.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.client.gui.updaters;
+
+import it.polimi.ingsw.client.gui.nodes.HResourceContainer;
+import it.polimi.ingsw.client.gui.nodes.ResourceContainer;
+import it.polimi.ingsw.client.model.ClientBaseStorage;
+import it.polimi.ingsw.client.model.ClientPlayer;
+import it.polimi.ingsw.client.observables.Listener;
+import it.polimi.ingsw.model.storage.BaseStorage;
+import javafx.application.Platform;
+
+public class StorageGuiUpdater implements Listener<ClientBaseStorage> {
+
+    private final ResourceContainer resourceContainer;
+
+    public StorageGuiUpdater(ResourceContainer resourceContainer, ClientBaseStorage clientBaseStorage) {
+        this.resourceContainer = resourceContainer;
+
+        clientBaseStorage.addListener(this);
+        update(clientBaseStorage);
+    }
+
+    @Override
+    public void update(ClientBaseStorage clientBaseStorage) {
+        Platform.runLater(() -> resourceContainer.setRawStorage(clientBaseStorage.getStorage()));
+    }
+}
