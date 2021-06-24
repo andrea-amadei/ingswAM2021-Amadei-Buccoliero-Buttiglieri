9e6a91ba13af4efb92e1fe035087fe2689a66f43
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-06-24 15:10:07 +0200

    Added ShelfGuiUpdater

---
+++ src/main/java/it/polimi/ingsw/client/gui/updaters/ShelfGuiUpdater.java
@@ -0,0 +1,51 @@
+package it.polimi.ingsw.client.gui.updaters;
+
+import it.polimi.ingsw.client.gui.nodes.ShelfBox;
+import it.polimi.ingsw.client.model.ClientShelf;
+import it.polimi.ingsw.client.observables.Listener;
+
+import java.util.List;
+
+public class ShelfGuiUpdater implements Listener<ClientShelf> {
+
+    private final ShelfBox shelfBox;
+
+    public ShelfGuiUpdater(ShelfBox shelfBox, ClientShelf clientShelf) {
+        this.shelfBox = shelfBox;
+
+        clientShelf.addListener(this);
+        update(clientShelf);
+    }
+
+    @Override
+    public void update(ClientShelf clientShelf) {
+        String resource;
+
+        try {
+            resource = List.copyOf(clientShelf.getStorage().getResources().keySet()).get(0);
+        } catch (IndexOutOfBoundsException e) {
+            shelfBox.setResource1("none");
+            shelfBox.setResource2("none");
+            shelfBox.setResource3("none");
+
+            return;
+        }
+
+        int n = clientShelf.getStorage().getResources().get(resource);
+
+        if(n >= 1)
+            shelfBox.setResource1(resource);
+        else
+            shelfBox.setResource1("none");
+
+        if(n >= 2)
+            shelfBox.setResource2(resource);
+        else
+            shelfBox.setResource2("none");
+
+        if(n >= 3)
+            shelfBox.setResource3(resource);
+        else
+            shelfBox.setResource3("none");
+    }
+}
