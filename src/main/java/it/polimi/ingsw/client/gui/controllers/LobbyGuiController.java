625feaa37d90228a296172d2b1a017dbf04f4463
Giorgio Natale Buttiglieri <10623930@polimi.it>
2021-06-25 16:33:50 +0200

    First scene launches the second one

--- src/main/java/it/polimi/ingsw/client/gui/controllers/LobbyGuiController.java
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/LobbyGuiController.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.client.gui.controllers;
 
+import it.polimi.ingsw.client.ClientGameBuilder;
+import it.polimi.ingsw.client.gui.GuiBuilder;
+import it.polimi.ingsw.client.model.ClientPlayer;
 import it.polimi.ingsw.common.payload_components.groups.setup.CreateMatchSetupPayloadComponent;
 import it.polimi.ingsw.common.payload_components.groups.setup.JoinMatchSetupPayloadComponent;
 import it.polimi.ingsw.common.payload_components.groups.setup.SetUsernameSetupPayloadComponent;
@@ -7,6 +10,9 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LobbyGuiController extends BaseController {
 
@@ -53,7 +59,7 @@ public class LobbyGuiController extends BaseController {
 
     @FXML
     private void startGameButtonAction() {
-
+        GuiBuilder.createGameScene(getSceneManager(), getModel(), getServerHandler());
     }
 
     @FXML
