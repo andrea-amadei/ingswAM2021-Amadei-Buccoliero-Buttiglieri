5ab6422d541068ea22325b704766c3bd4bdf4e3d
Giorgio Natale Buttiglieri <10623930@polimi.it>
2021-06-25 16:36:33 +0200

    Added handling of events

--- src/main/java/it/polimi/ingsw/client/gui/controllers/BoardGuiController.java
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/BoardGuiController.java
@@ -1,41 +1,22 @@
 package it.polimi.ingsw.client.gui.controllers;
 
+import it.polimi.ingsw.client.gui.beans.PreliminaryPickBean;
 import it.polimi.ingsw.client.gui.beans.ShopSelectionBean;
 import it.polimi.ingsw.client.gui.dialogs.ChooseCraftingDialog;
 import it.polimi.ingsw.client.gui.dialogs.CustomDialog;
+import it.polimi.ingsw.client.gui.dialogs.PreliminaryPickDialog;
 import it.polimi.ingsw.client.gui.events.*;
 import it.polimi.ingsw.client.gui.nodes.*;
 import it.polimi.ingsw.client.gui.updaters.*;
 import it.polimi.ingsw.client.model.ClientPlayer;
-import it.polimi.ingsw.client.model.ClientShelf;
-import it.polimi.ingsw.client.model.ConversionOption;
-import it.polimi.ingsw.exceptions.IllegalRawConversionException;
-import it.polimi.ingsw.exceptions.ParserException;
-import it.polimi.ingsw.gamematerials.FlagColor;
-import it.polimi.ingsw.gamematerials.LevelFlag;
-import it.polimi.ingsw.gamematerials.MarbleColor;
-import it.polimi.ingsw.gamematerials.ResourceTypeSingleton;
-import it.polimi.ingsw.model.fsm.states.ConversionSelectionState;
-import it.polimi.ingsw.model.holder.FaithHolder;
-import it.polimi.ingsw.model.market.ConversionActuator;
-import it.polimi.ingsw.model.market.Market;
-import it.polimi.ingsw.model.production.Crafting;
-import it.polimi.ingsw.model.production.CraftingCard;
-import it.polimi.ingsw.model.production.Production;
-import it.polimi.ingsw.parser.JSONParser;
-import it.polimi.ingsw.parser.raw.*;
-import it.polimi.ingsw.utils.ResourceLoader;
-import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import it.polimi.ingsw.common.payload_components.groups.PossibleActions;
+import it.polimi.ingsw.common.payload_components.groups.actions.*;
+import it.polimi.ingsw.model.actions.SelectPlayAction;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
-import java.io.InputStream;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -58,15 +39,22 @@ public class BoardGuiController extends BaseController {
     public PlayerNode currentPlayerNode;
     private Map<String, PlayerNode> playerNodes;
 
+    private String ownedUsername;
+
     public void initialize() {
         scoreboard = new ScoreboardBox();
         board.getChildren().add(scoreboard);
 
+        board.addEventFilter(ConfirmEvent.CONFIRM_EVENT, this::sendConfirmPayload);
+        board.addEventFilter(PreliminaryPickEvent.PRELIMINARY_PICK_EVENT_EVENT, this::onPreliminaryPickPressed);
         board.addEventFilter(ShopCardSelectionEvent.SHOP_CARD_SELECTION_EVENT, this::onShopCardSelection);
         board.addEventFilter(ConversionSelectionEvent.CONVERSION_SELECTION_EVENT, this::sendConversionOptionPayload);
         board.addEventFilter(MarketPickEvent.MARKET_PICK_EVENT, this::sendMarketPickPayload);
         board.addEventFilter(CraftingSelectionEvent.CRAFTING_SELECTION_EVENT, this::sendCraftingSelectionPayload);
         board.addEventFilter(LeaderInteractionEvent.LEADER_INTERACTION_EVENT, this::sendLeaderInteractionPayload);
+        board.addEventFilter(ChangedCurrentPlayerEvent.CHANGED_CURRENT_PLAYER_EVENT, this::onChangedCurrentPlayer);
+        board.addEventFilter(SwitchPlayerEvent.SWITCH_PLAYER_EVENT, this::onSwitchedShownPlayer);
+        board.addEventFilter(SelectPlayEvent.SELECT_PLAY_EVENT_EVENT, this::sendSelectPlayPayload);
     }
 
     public void boardSetup() {
@@ -76,6 +64,7 @@ public class BoardGuiController extends BaseController {
             PlayerNode playerNode = new PlayerNode();
             playerNode.setLayoutX(14d);
             playerNode.setLayoutY(342d);
+            playerNode.setPickOnBounds(false);
             playerNodes.put(p.getUsername(), playerNode);
         }
 
@@ -83,29 +72,48 @@ public class BoardGuiController extends BaseController {
         AnchorPane.setLeftAnchor(scoreboard, 20d);
         AnchorPane.setTopAnchor(scoreboard, 20d);
 
+        //--------------------------------------------------OFFLINE DEBUG------------------------------------//
+        if(getServerHandler() == null) {
+            getModel().getPersonalData().setPossibleActions(Set.of(PossibleActions.PRELIMINARY_PICK, PossibleActions.CONFIRM_TIDY));
+            getModel().setCurrentPlayer(getModel().getPlayers().get(0));
+        }
+        // show the self board
         // TODO: change to:
         // setActivePlayer(getModel().getPersonalData().getUsername());
-        setActivePlayer(getModel().getPlayers().get(0).getUsername());
+        String selfUsername = getModel().getPersonalData().getUsername();
+        if(selfUsername == null || selfUsername.equals("Unknown"))
+            selfUsername = getModel().getPlayers().get(0).getUsername();
+        setActivePlayer(selfUsername);
+
+        ownedUsername = selfUsername;
+
+        /*getModel().setCurrentPlayer(getModel().getPlayers().get(1));
+        getModel().getPlayers().get(1).getLeaderCards().changeCoveredCardsNumber(2);
+        try {
+            getModel().getPlayers().get(1).getLeaderCards().addLeaderCard(JSONParser.parseToRaw("{\"id\":2,\"name\":\"aaa\",\"points\":2,\"requirements\":[{\"type\":\"flag\",\"color\":\"BLUE\",\"amount\":1},{\"type\":\"flag\",\"color\":\"PURPLE\",\"amount\":1}],\"special_abilities\":[{\"type\":\"discount\",\"resource\":\"shield\",\"amount\":1}]}", RawLeaderCard.class));
+        } catch (ParserException | IllegalRawConversionException e) {
+            e.printStackTrace();
+        }*/
 
         // TODO: ADD ALL GLOBAL UPDATERS
         new ShopGuiUpdater(shop, getModel().getShop());
         new MarketGuiUpdater(market, getModel().getMarket());
+        new MenuGuiUpdater(buttons, getModel().getPersonalData());
+        new TurnGuiUpdater(buttons, getModel());
+        new PersonalDataGuiUpdater(messages, getSceneManager().getStage(), getModel().getPersonalData());
 
         // TODO: ADD ALL PLAYER SPECIFIC UPDATERS
-        System.out.println(getModel().getPlayers().size());
         for(int i = 0; i < getModel().getPlayers().size(); i++) {
             new FaithPathGuiUpdater(faithPath, getModel().getPlayers().get(i).getFaithPath(), i + 1);
-            new PersonalDataGuiUpdater(messages, getModel().getPersonalData());
             new FlagHolderGuiUpdater(scoreboard, getModel().getPlayers().get(i).getFlagHolder(), i + 1);
             new PlayerGuiUpdater(scoreboard, playerNodes.get(getModel().getPlayers().get(i).getUsername()).getCupboard(), getModel().getPlayers().get(i), i + 1);
             new StorageGuiUpdater(playerNodes.get(getModel().getPlayers().get(i).getUsername()).getChest(), getModel().getPlayers().get(i).getChest());
             new StorageGuiUpdater(playerNodes.get(getModel().getPlayers().get(i).getUsername()).getHand(), getModel().getPlayers().get(i).getHand());
             new StorageGuiUpdater(playerNodes.get(getModel().getPlayers().get(i).getUsername()).getBasket(), getModel().getPlayers().get(i).getMarketBasket());
             new ProductionGuiUpdater(playerNodes.get(getModel().getPlayers().get(i).getUsername()).getProduction(), getModel().getPlayers().get(i).getProduction());
+            new LeaderCardsGuiUpdater(playerNodes.get(getModel().getPlayers().get(i).getUsername()).getLeaders(), getModel().getPlayers().get(i).getLeaderCards());
         }
 
-        changeGlobalNodesControlsStatus(false);
-        changePlayerNodeControlsStatus(currentPlayerNode, false);
     }
 
     public void setActivePlayer(String username) {
@@ -126,9 +134,11 @@ public class BoardGuiController extends BaseController {
     private void changeGlobalNodesControlsStatus(boolean disable){
         shop.setAreControlsDisabled(disable);
         market.setAreControlsDisabled(disable);
+        buttons.setAreControlsDisabled(disable);
     }
 
 
+    //UI-Related event handlers
     private void onShopCardSelection(ShopCardSelectionEvent event){
         ShopSelectionBean bean = event.getCardSelectionBean();
         bean.setUpgradableSlotsCount(getModel().getPlayers().get(0).getProduction().getUpgradableCraftingNumber());
@@ -136,20 +146,60 @@ public class BoardGuiController extends BaseController {
         dialog.openDialog();
     }
 
+    private void onPreliminaryPickPressed(PreliminaryPickEvent evt){
+        CustomDialog preliminaryDialog = new PreliminaryPickDialog(getSceneManager().getStage(), this::sendPreliminaryPickPayload);
+        preliminaryDialog.openDialog();
+    }
 
+    private void onChangedCurrentPlayer(ChangedCurrentPlayerEvent event){
+        String selfUsername = getModel().getPersonalData().getUsername();
+
+        //TODO: remove this, we only use it for testing without connection to server
+        if(selfUsername == null || selfUsername.equals("Unknown")){
+            selfUsername = getModel().getPlayers().get(0).getUsername();
+        }
+
+        if(selfUsername.equals(event.getUsername())){
+            changePlayerNodeControlsStatus(playerNodes.get(selfUsername), false);
+            changeGlobalNodesControlsStatus(false);
+        }else{
+            changePlayerNodeControlsStatus(playerNodes.get(selfUsername), true);
+            changeGlobalNodesControlsStatus(true);
+        }
+    }
+
+    private void onSwitchedShownPlayer(SwitchPlayerEvent event){
+        setActivePlayer(event.getUsername());
+    }
+
+
+
+    //Action sending events
     private void sendCardSelectionPayload(ShopSelectionBean bean){
+        if(getServerHandler() != null){
+            getServerHandler().sendPayload(new SelectCardFromShopActionPayloadComponent(ownedUsername, bean.getRow(), bean.getCol(), bean.getUpgradableIndex()));
+        }
         System.out.println("Selected card (" + bean.getRow() + ", " + bean.getCol() + ") and upgradable crafting " + bean.getUpgradableIndex());
     }
 
     private void sendConversionOptionPayload(ConversionSelectionEvent event){
+        if(getServerHandler() != null){
+            getServerHandler().sendPayload(new SelectConversionsActionPayloadComponent(ownedUsername, event.getBean().getSelectedConversions()));
+        }
         System.out.println("Selected options: " + event.getBean().getSelectedConversions());
     }
 
     private void sendMarketPickPayload(MarketPickEvent event){
+        if(getServerHandler() != null){
+            getServerHandler().sendPayload(new BuyFromMarketActionPayloadComponent(ownedUsername, event.getBean().isRow(), event.getBean().getIndex()));
+        }
         System.out.println("Market Pick: " + event.getBean().isRow() + " " + event.getBean().getIndex());
     }
 
     private void sendCraftingSelectionPayload(CraftingSelectionEvent event){
+        if(getServerHandler() != null){
+            getServerHandler().sendPayload(new SelectCraftingActionPayloadComponent(ownedUsername, event.getBean().getCraftingType(), event.getBean().getIndex()));
+        }
         System.out.println("Crafting Selected: " + event.getBean().getCraftingType() + " " + event.getBean().getIndex());
     }
 
@@ -164,10 +214,45 @@ public class BoardGuiController extends BaseController {
             id = -1;
         }
 
-        if(isActivate)
+        if(isActivate) {
+            if(getServerHandler() != null){
+                getServerHandler().sendPayload(new ActivateLeaderActionPayloadComponent(ownedUsername, id));
+            }
             System.out.println("Activate Leader with id " + id + " (index: " + index + ")");
-        else
+        }
+        else {
+            if(getServerHandler() != null){
+                getServerHandler().sendPayload(new DiscardLeaderActionPayloadComponent(ownedUsername, id));
+            }
             System.out.println("Discarded Leader with id " + id + " (index: " + index + ")");
+        }
+    }
+
+    private void sendConfirmPayload(ConfirmEvent event){
+        Set<PossibleActions> possibleActions = getModel().getPersonalData().getPossibleActions();
+        if(possibleActions.contains(PossibleActions.CONFIRM)){
+            if(getServerHandler() != null){
+                getServerHandler().sendPayload(new ConfirmActionPayloadComponent(ownedUsername));
+            }
+            System.out.println("Confirmed");
+        }else if(possibleActions.contains(PossibleActions.CONFIRM_TIDY)){
+            if(getServerHandler() != null){
+                getServerHandler().sendPayload(new ConfirmTidyActionPayloadComponent(ownedUsername));
+            }
+            System.out.println("ConfirmedTidy");
+        }
+    }
+
+    private void sendPreliminaryPickPayload(PreliminaryPickBean bean){
+        if(getServerHandler() != null)
+            getServerHandler().sendPayload(new PreliminaryPickActionPayloadComponent(ownedUsername, bean.getLeaderIndexes(), bean.getSelectedResources()));
+        System.out.println("PreliminaryPick: " + bean.getLeaderIndexes() + " " + bean.getSelectedResources());
+    }
+
+    private void sendSelectPlayPayload(SelectPlayEvent event){
+        if(getServerHandler() != null)
+            getServerHandler().sendPayload(new SelectPlayActionPayloadComponent(ownedUsername, event.getPlay()));
+        System.out.println("Selected play: " + event.getPlay());
     }
 
 }
