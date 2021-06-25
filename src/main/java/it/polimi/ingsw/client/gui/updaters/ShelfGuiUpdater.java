3529df2bcd17a5d41afb9d815540bc57ce3053a0
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-06-25 16:09:59 +0200

    Fixed ShelfGuiUpdater's updated which didn't use Platform.runLater method to update GUI

--- src/main/java/it/polimi/ingsw/client/gui/updaters/ShelfGuiUpdater.java
+++ src/main/java/it/polimi/ingsw/client/gui/updaters/ShelfGuiUpdater.java
@@ -25,9 +25,11 @@ public class ShelfGuiUpdater implements Listener<ClientShelf> {
         try {
             resource = List.copyOf(clientShelf.getStorage().getResources().keySet()).get(0);
         } catch (IndexOutOfBoundsException e) {
-            shelfBox.setResource1("none");
-            shelfBox.setResource2("none");
-            shelfBox.setResource3("none");
+            Platform.runLater(() -> {
+                shelfBox.setResource1("none");
+                shelfBox.setResource2("none");
+                shelfBox.setResource3("none");
+            });
 
             return;
         }
@@ -35,20 +37,20 @@ public class ShelfGuiUpdater implements Listener<ClientShelf> {
         int n = clientShelf.getStorage().getResources().get(resource);
 
         if(n >= 1)
-            shelfBox.setResource1(resource);
+            Platform.runLater(() -> shelfBox.setResource1(resource));
         else
-            shelfBox.setResource1("none");
+            Platform.runLater(() -> shelfBox.setResource1("none"));
 
         if(n >= 2)
-            shelfBox.setResource2(resource);
+            Platform.runLater(() -> shelfBox.setResource2(resource));
         else
-            shelfBox.setResource2("none");
+            Platform.runLater(() -> shelfBox.setResource2("none"));
 
         if(n >= 3)
-            shelfBox.setResource3(resource);
+            Platform.runLater(() -> shelfBox.setResource3(resource));
         else
-            shelfBox.setResource3("none");
+            Platform.runLater(() -> shelfBox.setResource3("none"));
 
-        shelfBox.setSelectedResources(clientShelf.getSelectedResources().values().stream().reduce(0, Integer::sum));
+        Platform.runLater(() -> shelfBox.setSelectedResources(clientShelf.getSelectedResources().values().stream().reduce(0, Integer::sum)));
     }
 }
