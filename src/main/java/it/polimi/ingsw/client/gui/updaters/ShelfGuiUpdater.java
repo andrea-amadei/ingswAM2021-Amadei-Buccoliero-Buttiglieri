59b90c1ec8d9a08d139e6cdd68ded01c1fb8b3c9
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-06-26 19:17:21 +0200

    Fixed shelves not updating correctly when unselecting resources

--- src/main/java/it/polimi/ingsw/client/gui/updaters/ShelfGuiUpdater.java
+++ src/main/java/it/polimi/ingsw/client/gui/updaters/ShelfGuiUpdater.java
@@ -20,37 +20,37 @@ public class ShelfGuiUpdater implements Listener<ClientShelf> {
 
     @Override
     public void update(ClientShelf clientShelf) {
-        String resource;
+        Platform.runLater(() -> {
+            String resource;
 
-        try {
-            resource = List.copyOf(clientShelf.getStorage().getResources().keySet()).get(0);
-        } catch (IndexOutOfBoundsException e) {
-            Platform.runLater(() -> {
+            shelfBox.setSelectedResources(clientShelf.getSelectedResources().values().stream().reduce(0, Integer::sum));
+
+            try {
+                resource = List.copyOf(clientShelf.getStorage().getResources().keySet()).get(0);
+            } catch (IndexOutOfBoundsException e) {
                 shelfBox.setResource1("none");
                 shelfBox.setResource2("none");
                 shelfBox.setResource3("none");
-            });
 
-            return;
-        }
+                return;
+            }
 
-        int n = clientShelf.getStorage().getResources().get(resource);
+            int n = clientShelf.getStorage().getResources().get(resource);
 
-        if(n >= 1)
-            Platform.runLater(() -> shelfBox.setResource1(resource));
-        else
-            Platform.runLater(() -> shelfBox.setResource1("none"));
+            if (n >= 1)
+                shelfBox.setResource1(resource);
+            else
+                shelfBox.setResource1("none");
 
-        if(n >= 2)
-            Platform.runLater(() -> shelfBox.setResource2(resource));
-        else
-            Platform.runLater(() -> shelfBox.setResource2("none"));
+            if (n >= 2)
+                shelfBox.setResource2(resource);
+            else
+                shelfBox.setResource2("none");
 
-        if(n >= 3)
-            Platform.runLater(() -> shelfBox.setResource3(resource));
-        else
-            Platform.runLater(() -> shelfBox.setResource3("none"));
-
-        Platform.runLater(() -> shelfBox.setSelectedResources(clientShelf.getSelectedResources().values().stream().reduce(0, Integer::sum)));
+            if (n >= 3)
+                shelfBox.setResource3(resource);
+            else
+                shelfBox.setResource3("none");
+        });
     }
 }
