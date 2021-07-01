ec53649b860112cbc9f7d8016366edadddfad092
andrea-amadei <andrea1.amadei@mail.polimi.it>
2021-07-01 11:56:45 +0200

    Added shared methods in interface

--- src/main/java/it/polimi/ingsw/configurator/nodes/AbilitySelector.java
+++ src/main/java/it/polimi/ingsw/configurator/nodes/AbilitySelector.java
@@ -1,4 +1,8 @@
 package it.polimi.ingsw.configurator.nodes;
 
+import it.polimi.ingsw.common.parser.raw.RawSpecialAbility;
+
 public interface AbilitySelector {
+    RawSpecialAbility getRawSpecialAbility();
+    void setRawSpecialAbility(RawSpecialAbility rawSpecialAbility);
 }
--- src/main/java/it/polimi/ingsw/configurator/nodes/RequirementSelector.java
+++ src/main/java/it/polimi/ingsw/configurator/nodes/RequirementSelector.java
@@ -1,4 +1,8 @@
 package it.polimi.ingsw.configurator.nodes;
 
+import it.polimi.ingsw.common.parser.raw.RawRequirement;
+
 public interface RequirementSelector {
+    RawRequirement getRawRequirement();
+    void setRawRequirement(RawRequirement rawRequirement);
 }
